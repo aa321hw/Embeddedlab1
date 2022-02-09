9208406be759b9aa77c8c082711a840e8255ec4f
aa321hw <72445338+aa321hw@users.noreply.github.com>
2022-02-09 14:20:25 +0000

    Add files via upload

---
+++ ES_asses1.ino
@@ -0,0 +1,56 @@
+int mult = 1;
+
+int SIGNALAPIN = 8;
+int SIGNALBPIN = 9;
+int a = 100 * mult;
+int b = 1200 * mult;
+int c = 9 * mult;
+int d = 6500 * mult;
+int increase = 50 *mult;
+int mode = 1;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(SIGNALAPIN,OUTPUT);
+  pinMode(SIGNALBPIN,OUTPUT);
+   
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(SIGNALBPIN,HIGH);
+  delay(500);
+  digitalWrite(SIGNALBPIN,LOW);
+  int count = 0;
+
+  if (mode == 1){
+    
+    for (int i = a;count < c;i = i + increase){
+
+    digitalWrite(SIGNALAPIN,HIGH);
+    delay(i);
+    digitalWrite(SIGNALAPIN,LOW);
+    delay(b);
+    count = count + 1;
+    
+    }
+  }
+
+  if (mode == 2){
+
+    for (int i = a * c;count < c;i = i - increase){
+
+    digitalWrite(SIGNALAPIN,HIGH);
+    delay(i);
+    digitalWrite(SIGNALAPIN,LOW);
+    delay(b);
+    count = count + 1;
+    
+    }
+  }
+
+  delay(d);
+
+  
+}
