b61054eb997ff3ebc4dbaaf2255fa3f6c6238aac
aa321hw <72445338+aa321hw@users.noreply.github.com>
2022-02-10 20:28:48 +0000

    Add files via upload

--- ES_asses1.ino
+++ ES_asses1.ino
@@ -1,16 +1,21 @@
-int mult = 10 * 2;
+int mult = 10 * 2; //Variable used to increase times
 
+//Define pins
 int SIGNALAPIN = 8;
 int SIGNALBPIN = 9;
+
+//Define variables for the wave
 int a = 0.1 * mult;
 int b = 1.2 * mult;
 int c = 9;
 int d = 6.5 * mult;
 int increase = 0.05 *mult;
-int signalbdelay = 1 * 2; 
+int signalbdelay = 0.05 * mult; 
 
-int mode = 2;
+//Set the mode
+int mode = 1;
 
+//Setup of pins to output
 void setup() {
   // put your setup code here, to run once:
   pinMode(SIGNALAPIN,OUTPUT);
@@ -25,18 +30,21 @@ void loop() {
   digitalWrite(SIGNALBPIN,HIGH);
   delay(signalbdelay);
   digitalWrite(SIGNALBPIN,LOW);
+
+  //Create variable count to track the pulses
   int count = 0;
 
   //Signal A in normal mode
   if (mode == 1){
-    
+
+    //Create each pulse increasing the delay
     for (int i = a;count < c;i = i + increase){
 
-    digitalWrite(SIGNALAPIN,HIGH);
-    delay(i);
-    digitalWrite(SIGNALAPIN,LOW);
-    delay(b);
-    count = count + 1;
+      digitalWrite(SIGNALAPIN,HIGH);
+      delay(i);
+      digitalWrite(SIGNALAPIN,LOW);
+      delay(b);
+      count = count + 1;
     
     }
   }
@@ -44,17 +52,17 @@ void loop() {
   //Signal A in alternative mode
   if (mode == 2){
 
+    //Create the pulse decreasing the delay
     for (int i = a + (increase * (c-1));count < c;i = i - increase){
 
-    digitalWrite(SIGNALAPIN,HIGH);
-    delay(i);
-    digitalWrite(SIGNALAPIN,LOW);
-    delay(b);
-    count = count + 1;
+      digitalWrite(SIGNALAPIN,HIGH);
+      delay(i);
+      digitalWrite(SIGNALAPIN,LOW);
+      delay(b);
+      count = count + 1;
     
     }
   }
-
   delay(d);
 
   
